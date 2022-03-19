de09e780ae89778c515aac6fa757e8d339e08798
Colin Finger <finger.colin@gmail.com>
2022-03-19 10:07:04 +0100

    ~

--- myorg/apps/api/src/app/app.service.ts
+++ myorg/apps/api/src/app/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from '@myorg/data';
 
 @Injectable()
 export class AppService {
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
   getData(): { message: string } {
     return { message: 'Welcome to api!' };
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
   }
 }
